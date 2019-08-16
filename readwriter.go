fcadf21a7a875851e37955ee8bbd61bd1876a7b8
lixz <lixz@dns.com>
2019-08-16 17:14:24 +0800

    keep Limiter

--- readwriter.go
+++ readwriter.go
@@ -7,6 +7,7 @@ import (
 )
 
 type RateReadWriter struct {
+	Limit *rate.Limiter
 	io.Reader
 	io.Writer
 }
@@ -15,11 +16,11 @@ func NewRateReadWriter(rw io.ReadWriter, l rate.Limit, burst int) io.ReadWriter 
 	limit := rate.NewLimiter(l, burst)
 	r := AddRateReader(rw, limit)
 	w := AddRateWriter(rw, limit)
-	return &RateReadWriter{r, w}
+	return &RateReadWriter{Limit: limit, Reader: r, Writer: w}
 }
 
 func AddRateReadWriter(rw io.ReadWriter, limit *rate.Limiter) io.ReadWriter {
 	r := AddRateReader(rw, limit)
 	w := AddRateWriter(rw, limit)
-	return &RateReadWriter{r, w}
+	return &RateReadWriter{Limit: limit, Reader: r, Writer: w}
 }
